4ec10cb3de72f07c4b231121d95c8dbf48e6c807
marcprux <mwp1@cornell.edu>
2023-08-28 11:24:30 -0400

    Add color pixmap tests

--- Package.swift
+++ Package.swift
@@ -9,9 +9,9 @@ let package = Package(
         .library(name: "SkipUIKt", targets: ["SkipUIKt"]),
     ],
     dependencies: [ 
-        .package(url: "https://source.skip.tools/skip.git", from: "0.6.4"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.6.5"),
         .package(url: "https://source.skip.tools/skip-unit.git", from: "0.1.0"),
-        .package(url: "https://source.skip.tools/skip-lib.git", from: "0.2.0"),
+        .package(url: "https://source.skip.tools/skip-lib.git", from: "0.2.1"),
         .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.0.14"),
     ],
     targets: [
