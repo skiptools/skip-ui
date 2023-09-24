1e00627627a734354a512d4222288dd4b5295acf
marcprux <mwp1@cornell.edu>
2023-09-24 16:39:30 -0400

    Dependency bump

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "SkipUI", targets: ["SkipUI"]),
     ],
     dependencies: [ 
-        .package(url: "https://source.skip.tools/skip.git", from: "0.6.72"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.6.75"),
         .package(url: "https://source.skip.tools/skip-model.git", from: "0.2.1"),
     ],
     targets: [
