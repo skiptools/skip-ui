54377d542e78496d1734181291b7d3c69842050e
marcprux <mwp1@cornell.edu>
2023-11-19 08:50:43 -0500

    Update source headers

--- Sources/SkipUI/Skip/ItemPosition.kt
+++ Sources/SkipUI/Skip/ItemPosition.kt
@@ -1,3 +1,6 @@
+// This is free software: you can redistribute and/or modify it
+// under the terms of the GNU Lesser General Public License 3.0
+// as published by the Free Software Foundation https://fsf.org
 package org.burnoutcrew.reorderable
 
 data class ItemPosition(val index: Int, val key: Any?)
--- Sources/SkipUI/Skip/ReorderableItem.kt
+++ Sources/SkipUI/Skip/ReorderableItem.kt
@@ -1,3 +1,9 @@
+// This is free software: you can redistribute and/or modify it
+// under the terms of the GNU Lesser General Public License 3.0
+// as published by the Free Software Foundation https://fsf.org
+//
+// This file uses code publised with the following header:
+//
 /*
  * Copyright 2022 André Claßen
  *
