efa084f6358920194a47279aafa6e08760db5049
Abe White <abewhite@mac.com>
2023-12-11 13:17:35 -0600

    Update skip dependency

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "SkipUI", targets: ["SkipUI"]),
     ],
     dependencies: [ 
-        .package(url: "https://source.skip.tools/skip.git", from: "0.7.31"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.7.32"),
         .package(url: "https://source.skip.tools/skip-model.git", from: "0.3.2"),
     ],
     targets: [
