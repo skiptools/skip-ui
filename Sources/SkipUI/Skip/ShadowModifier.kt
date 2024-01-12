c6a7b6aea1dcdd4d2480678bacc3b575ccef9249
Abe White <abewhite@mac.com>
2024-01-12 12:27:25 -0600

    Simplify and comment shadow implementation

--- Sources/SkipUI/Skip/ShadowModifier.kt
+++ Sources/SkipUI/Skip/ShadowModifier.kt
@@ -4,14 +4,11 @@
 // under the terms of the GNU Lesser General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
 
-// This is based on code from:
-// https://gist.github.com/Andrew0000/3edb9c25ebc20a2935c9ff4805e05f5d
-
 package skip.ui
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.BlurredEdgeTreatment
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.drawWithContent
@@ -25,45 +22,43 @@ import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import kotlin.math.ceil
 
 /// Compose the given content with a drop shadow on all non-transparent pixels.
 @Composable fun Shadowed(context: ComposeContext, color: Color, offsetX: Dp, offsetY: Dp, blurRadius: Dp, content: @Composable (ComposeContext) -> Unit) {
-    val contentContext = context.content()
     val density = LocalDensity.current
     val offsetXPx = with(density) { offsetX.toPx() }.toInt()
     val offsetYPx = with(density) { offsetY.toPx() }.toInt()
-    // Pad by the shadow layer by the radius to prevent clipping the blur
-    val paddingPx = ceil(with(density) { blurRadius.toPx() }).toInt()
-    Layout(modifier = context.modifier, content = {
-        // Render content normally
-        content(contentContext)
-        ComposeContainer(fixedWidth = true, fixedHeight = true) { modifier ->
-            Box(modifier = modifier
-                .drawWithContent {
-                    val matrix = shadowColorMatrix(color)
-                    val filter = ColorFilter.colorMatrix(matrix)
-                    val paint = Paint().apply {
-                        colorFilter = filter
-                    }
-                    drawIntoCanvas { canvas ->
-                        canvas.saveLayer(Rect(0f, 0f, size.width, size.height), paint)
-                        drawContent()
-                        canvas.restore()
-                    }
-                }
-                .blur(radius = blurRadius, BlurredEdgeTreatment.Unbounded)
-                .padding(all = blurRadius)
-            ) {
-                content(contentContext)
+    val blurRadiusPx = ceil(with(density) { blurRadius.toPx() }).toInt()
+
+    val contentContext = context.content()
+    val shadowContext = context.content(modifier = Modifier
+        .drawWithContent {
+            val matrix = shadowColorMatrix(color)
+            val filter = ColorFilter.colorMatrix(matrix)
+            val paint = Paint().apply {
+                colorFilter = filter
+            }
+            drawIntoCanvas { canvas ->
+                // Paint content again with shadow color
+                canvas.saveLayer(Rect(0f, 0f, size.width, size.height), paint)
+                drawContent()
+                canvas.restore()
             }
         }
+        .blur(radius = blurRadius, BlurredEdgeTreatment.Unbounded)
+        .padding(all = blurRadius) // Pad to prevent clipping blur
+    )
+
+    Layout(modifier = context.modifier, content = {
+        content(contentContext) // Render normally
+        content(shadowContext) // Render as shadow
     }) { measurables, constraints ->
+        // Allow shadow to extend beyond bounds without affecting layout
         val contentPlaceable = measurables[0].measure(constraints)
-        val shadowPlaceable = measurables[1].measure(Constraints(maxWidth = contentPlaceable.width + paddingPx * 2, maxHeight = contentPlaceable.height + paddingPx * 2))
+        val shadowPlaceable = measurables[1].measure(Constraints(maxWidth = contentPlaceable.width + blurRadiusPx * 2, maxHeight = contentPlaceable.height + blurRadiusPx * 2))
         layout(width = contentPlaceable.width, height = contentPlaceable.height) {
-            shadowPlaceable.placeRelative(x = offsetXPx - paddingPx, y = offsetYPx - paddingPx)
+            shadowPlaceable.placeRelative(x = offsetXPx - blurRadiusPx, y = offsetYPx - blurRadiusPx)
             contentPlaceable.placeRelative(x = 0, y = 0)
         }
     }
@@ -79,6 +74,6 @@ private fun shadowColorMatrix(color: Color): ColorMatrix {
         set(0, 4, color.red * 255) // Use given color's R
         set(1, 4, color.green * 255) // Use given color's G
         set(2, 4, color.blue * 255) // Use given color's B
-        set(3, 3, color.alpha) // Multiply original alpha by shadow alpha
+        set(3, 3, color.alpha) // Multiply original alpha by shadow color alpha
     }
 }
