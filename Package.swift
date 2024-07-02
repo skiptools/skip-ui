2a085baa7e5655dedae9cf156eedd86eac62179b
Abe White <abewhite@mac.com>
2024-07-02 15:10:13 -0500

    Bump required transpiler version

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "SkipUI", targets: ["SkipUI"]),
     ],
     dependencies: [ 
-        .package(url: "https://source.skip.tools/skip.git", from: "0.8.46"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.9.1"),
         .package(url: "https://source.skip.tools/skip-model.git", from: "0.7.0"),
     ],
     targets: [
