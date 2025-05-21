1ff49caeeadfd06b64770de5517c1d223ce4d3ee
Dan Fabulich <dan@fabulich.com>
2025-05-20 21:04:01 -0700

    Add stub declaration for onPreferenceChange
    This was surprisingly tricky. The SwiftUI declaration says:
    
    ```swift
    public func onPreferenceChange<K>(_ key: K.Type = K.self, perform action: @escaping (K.Value) -> Void) -> some View where K : PreferenceKey, K.Value : Equatable
    ```
    
    But the Skip transpiler doesn't like that. "Skip does not support the referenced type as a generic constraint"
    
    To my surprise, the Skip transpiler refuses to like it even when I add a `//SKIP DECLARE` directive.
    
    So, I commented out the K.Value : Equatable constraint. (Xcode's compiler will enforce it anyway.)

--- Sources/SkipUI/SkipUI/Environment/PreferenceKey.swift
+++ Sources/SkipUI/SkipUI/Environment/PreferenceKey.swift
@@ -168,16 +168,23 @@ struct PreferenceNode<Value>: Equatable {
 
 extension View {
     public func preference(key: Any.Type, value: Any) -> any View {
-        #if SKIP
+#if SKIP
         return ComposeModifierView(targetView: self) {
             PreferenceValues.shared.contribute(context: $0, key: key, value: value)
             return ComposeResult.ok
         }
-        #else
+#else
         return self
-        #endif
+#endif
+    }
+    
+    // TODO: Add Equatable constraint to K.Value
+    // SKIP DECLARE: public fun <K: PreferenceKey<V>, V: Any> View.onPreferenceChange(key: KClass<K>, perform: (V) -> Unit): View
+    public func onPreferenceChange<K>(_ key: K.Type = K.self, perform action: @escaping (K.Value) -> Void) -> some View where K : PreferenceKey/*, K.Value : Equatable*/ {
+        return self
     }
 }
+
 
 #if false
 @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
@@ -316,23 +323,6 @@ extension View {
     ///
     /// - Returns: a new version of the view that writes the preference.
     public func anchorPreference<A, K>(key _: K.Type = K.self, value: Anchor<A>.Source, transform: @escaping (Anchor<A>) -> K.Value) -> some View where K : PreferenceKey { return stubView() }
-
-}
-
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
-extension View {
-
-    /// Adds an action to perform when the specified preference key's value
-    /// changes.
-    ///
-    /// - Parameters:
-    ///   - key: The key to monitor for value changes.
-    ///   - action: The action to perform when the value for `key` changes. The
-    ///     `action` closure passes the new value as its parameter.
-    ///
-    /// - Returns: A view that triggers `action` when the value for `key`
-    ///   changes.
-    public func onPreferenceChange<K>(_ key: K.Type = K.self, perform action: @escaping (K.Value) -> Void) -> some View where K : PreferenceKey, K.Value : Equatable { return stubView() }
 
 }
 
