4fc51ea071b0aba022fafa86c108743ccd72151d
fhasse95 <49185957+fhasse95@users.noreply.github.com>
2026-01-06 03:31:56 +0100

    Added text field selection support (#282)
    * Added text field selection support
    
    * Update TextField.swift
    
    * Code Review Comments
    
    * Update TextSelection.swift
    
    * Update TextSelection.swift
    
    * Update TextField.swift

---
+++ Sources/SkipUI/SkipUI/Text/TextSelection.swift
@@ -0,0 +1,56 @@
+// Copyright 2023–2025 Skip
+// SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
+#if !SKIP_BRIDGE
+import Foundation
+#if SKIP
+import androidx.compose.ui.text.TextRange
+#endif
+
+// SKIP @bridge
+public struct TextSelection : Equatable, Hashable {
+    public enum Indices : Equatable, Hashable {
+        case selection(Range<String.Index>)
+    }
+    
+    public var indices: TextSelection.Indices
+    
+    public var affinity: TextSelectionAffinity {
+        return .automatic
+    }
+    
+    public let isInsertion: Bool
+    
+    public init(range: Range<String.Index>) {
+        self.indices = .selection(range)
+        self.isInsertion = false
+    }
+    
+    public init(insertionPoint: String.Index) {
+        self.indices = .selection(insertionPoint..<insertionPoint)
+        self.isInsertion = true
+    }
+    
+    public static func == (lhs: TextSelection, rhs: TextSelection) -> Bool {
+        return false
+    }
+    
+    #if SKIP
+    
+    /// Return the equivalent Compose text range.
+    public func asComposeTextRange() -> TextRange {
+        switch self.indices {
+        case .selection(let range):
+            return TextRange(range.lowerBound, range.upperBound)
+        }
+    }
+    
+    #endif
+}
+
+public enum TextSelectionAffinity : Equatable, Hashable {
+    case automatic
+    case upstream
+    case downstream
+}
+
+#endif
